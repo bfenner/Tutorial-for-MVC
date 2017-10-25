88dfa3f1e56b0ca04f45399bbc1d33bac85499be
bfenner <bruno.r.fenner@gmail.com>
2017-10-25 17:59:36 +0200

    Für denn Aufbau einer Verbindung zum SQL

--- Selbstbau/inc/connect.inc.php
+++ Selbstbau/inc/connect.inc.php
@@ -1,5 +1,12 @@
 <?php
-function findePersonen(){
-	$person 
-}
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	
+	$conn = new mysqli($servername, $username, $password);
+	
+	if($conn->connect_error){
+		die("Connection failed: " $conn->connect_error);
+	}
+	echo "Connected successfully";
  ?>
